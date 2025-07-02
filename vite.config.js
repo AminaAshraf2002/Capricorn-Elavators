c9f4222a015cdd3e9eb25aa1e8aec79a6dcd4514
AminaAshraf2002 <aminaashraf246@gmail.com>
2025-07-02 12:58:48 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          animations: ['gsap', 'aos'],  
+          routing: ['react-router-dom'],
+          icons: ['lucide-react']
+          // Remove hotspots from manual chunks
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      'prop-types': 'prop-types'
+    }
+  }
 })
